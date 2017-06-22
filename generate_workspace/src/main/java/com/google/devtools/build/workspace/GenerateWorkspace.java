5b5d450f3617bcc0acf9210abf7d044315aec5d3
Kristina <k.chodorow@gmail.com>
2017-06-22 10:54:14 -0400

    Print out args so it's easy to rerun generate_workspace (#36)
    We've heard from users who run generate_workspace multiple times.
    Keeping a record of the command run should make it easier to
    regenerate the WORKSPACE file and slightly modify the input, if
    needed.

--- generate_workspace/src/main/java/com/google/devtools/build/workspace/GenerateWorkspace.java
+++ generate_workspace/src/main/java/com/google/devtools/build/workspace/GenerateWorkspace.java
@@ -57,7 +57,7 @@ public class GenerateWorkspace {
 
     try {
       GenerateWorkspace workspaceFileGenerator = new GenerateWorkspace(
-          options.outputDir, options.directToWorkspace);
+          args, options.outputDir, options.directToWorkspace);
       workspaceFileGenerator.generateFromPom(options.mavenProjects);
       workspaceFileGenerator.generateFromArtifacts(options.artifacts);
       workspaceFileGenerator.writeResults();
@@ -67,12 +67,13 @@ public class GenerateWorkspace {
     }
   }
 
-  private GenerateWorkspace(String outputDirStr, boolean directToWorkspace) throws IOException {
+  private GenerateWorkspace(String[] args, String outputDirStr, boolean directToWorkspace)
+      throws IOException {
     this.resolver = new Resolver(new DefaultModelResolver());
     this.inputs = Lists.newArrayList();
     this.resultWriter = directToWorkspace
-        ? new WorkspaceWriter(outputDirStr)
-        : new BzlWriter(outputDirStr);
+        ? new WorkspaceWriter(args, outputDirStr)
+        : new BzlWriter(args, outputDirStr);
   }
 
   private void generateFromPom(List<String> projects) {
