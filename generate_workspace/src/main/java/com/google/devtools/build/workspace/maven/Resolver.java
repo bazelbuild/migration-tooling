942c0a06daf5e82bb1c7c84e46520455fc0868a7
Matt Weiden <weiden@soundcloud.com>
2017-10-31 11:10:58 -0700

    Add sha1 and repo to root dependency

--- generate_workspace/src/main/java/com/google/devtools/build/workspace/maven/Resolver.java
+++ generate_workspace/src/main/java/com/google/devtools/build/workspace/maven/Resolver.java
@@ -22,26 +22,19 @@ import com.google.common.collect.Sets;
 import com.google.common.io.CharStreams;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.invoke.MethodHandles;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.logging.Logger;
 import javax.annotation.Nullable;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
 import org.apache.maven.artifact.versioning.VersionRange;
-import org.apache.maven.model.Dependency;
-import org.apache.maven.model.Exclusion;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.Parent;
-import org.apache.maven.model.Repository;
+import org.apache.maven.model.*;
 import org.apache.maven.model.building.DefaultModelProcessor;
 import org.apache.maven.model.building.FileModelSource;
 import org.apache.maven.model.building.ModelSource;
@@ -51,19 +44,17 @@ import org.apache.maven.model.resolution.UnresolvableModelException;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.util.artifact.JavaScopes;
 
-/**
- * Resolves Maven dependencies.
- */
+/** Resolves Maven dependencies. */
 public class Resolver {
 
-  private static final Logger logger = Logger.getLogger(
-      MethodHandles.lookup().lookupClass().getName());
+  private static final Logger logger =
+      Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
   private static final String TOP_LEVEL_ARTIFACT = "pom.xml";
 
   /**
-   * The set of scopes whose artifacts are pulled into the transitive dependency tree.
-   * See https://maven.apache.org/guides/introduction/introduction-to-dependency-mechanism.html for
-   * more details on how maven handles this.
+   * The set of scopes whose artifacts are pulled into the transitive dependency tree. See
+   * https://maven.apache.org/guides/introduction/introduction-to-dependency-mechanism.html for more
+   * details on how maven handles this.
    */
   private static final Set<String> INHERITED_SCOPES =
       Sets.newHashSet(JavaScopes.COMPILE, JavaScopes.RUNTIME);
@@ -95,19 +86,18 @@ public class Resolver {
   }
 
   public Resolver(DefaultModelResolver resolver, List<Rule> aliases) {
-    this(resolver, VersionResolver.defaultResolver(), aliases);
+    this(resolver, resolver.getVersionResolver(), aliases);
   }
 
-  /**
-   * Returns all maven_jars.
-   */
+  /** Returns all maven_jars. */
   public Collection<Rule> getRules() {
     return deps.values();
   }
 
   /**
-   * Given a local path to a Maven project, this attempts to find the transitive dependencies of
-   * the project.
+   * Given a local path to a Maven project, this attempts to find the transitive dependencies of the
+   * project.
+   *
    * @param projectPath The path to search for Maven projects.
    * @param scopes The scopes to look up dependencies in.
    */
@@ -127,9 +117,7 @@ public class Resolver {
     return pom.getAbsolutePath();
   }
 
-  /**
-   * Resolves an artifact as a root of a dependency graph.
-   */
+  /** Resolves an artifact as a root of a dependency graph. */
   public void resolveArtifact(String artifactCoord) {
     Artifact artifact;
     ModelSource modelSource;
@@ -142,21 +130,28 @@ public class Resolver {
     }
 
     Rule rule = new Rule(artifact);
-    deps.put(rule.name(), rule);  // add the artifact rule to the workspace
+    rule.setRepository(modelSource.getLocation());
+    rule.setSha1(downloadSha1(rule));
+    deps.put(rule.name(), rule); // add the artifact rule to the workspace
     Model model = modelResolver.getEffectiveModel(modelSource);
     if (model != null) {
       traverseDeps(model, Sets.newHashSet(), Sets.newHashSet(), rule);
     }
   }
-  
+
   /**
    * Resolves all dependencies from a given "model source," which could be either a URL or a local
    * file.
    */
   @VisibleForTesting
   void traverseDeps(Model model, Set<String> scopes, Set<String> exclusions, Rule parent) {
-    logger.info("\tDownloading pom for " + model.getGroupId() + ":"
-        + model.getArtifactId() + ":" + model.getVersion());
+    logger.info(
+        "\tDownloading pom for "
+            + model.getGroupId()
+            + ":"
+            + model.getArtifactId()
+            + ":"
+            + model.getVersion());
     for (Repository repo : model.getRepositories()) {
       modelResolver.addRepository(repo);
     }
@@ -205,15 +200,18 @@ public class Resolver {
       return;
     }
     try {
-      Rule artifactRule = new Rule(ArtifactBuilder.fromMavenDependency(dependency, versionResolver));
+      Rule artifactRule =
+          new Rule(ArtifactBuilder.fromMavenDependency(dependency, versionResolver));
       HashSet<String> localDepExclusions = Sets.newHashSet(exclusions);
-      dependency.getExclusions().forEach(
-          exclusion -> localDepExclusions.add(unversionedCoordinate(exclusion)));
+      dependency
+          .getExclusions()
+          .forEach(exclusion -> localDepExclusions.add(unversionedCoordinate(exclusion)));
 
       boolean isNewDependency = addArtifact(artifactRule, model.toString());
       if (isNewDependency) {
-        ModelSource depModelSource = modelResolver.resolveModel(
-            dependency.getGroupId(), dependency.getArtifactId(), dependency.getVersion());
+        ModelSource depModelSource =
+            modelResolver.resolveModel(
+                dependency.getGroupId(), dependency.getArtifactId(), dependency.getVersion());
         if (depModelSource != null) {
           artifactRule.setRepository(depModelSource.getLocation());
           artifactRule.setSha1(downloadSha1(artifactRule));
@@ -233,15 +231,19 @@ public class Resolver {
         parent.getDependencies().addAll(artifactRule.getDependencies());
       }
     } catch (UnresolvableModelException | InvalidArtifactCoordinateException e) {
-      logger.warning("Could not resolve dependency " + dependency.getGroupId()
-          + ":" + dependency.getArtifactId() + ":" + dependency.getVersion() + ": "
-          + e.getMessage());
+      logger.warning(
+          "Could not resolve dependency "
+              + dependency.getGroupId()
+              + ":"
+              + dependency.getArtifactId()
+              + ":"
+              + dependency.getVersion()
+              + ": "
+              + e.getMessage());
     }
   }
 
-  /**
-   * Find the POM files for a given pom's parent(s) and submodules.
-   */
+  /** Find the POM files for a given pom's parent(s) and submodules. */
   private void resolveSourceLocations(FileModelSource fileModelSource) {
     Model model = modelResolver.getRawModel(fileModelSource);
     if (model == null) {
@@ -253,8 +255,7 @@ public class Resolver {
     if (model.getGroupId() == null) {
       model.setGroupId(parent.getGroupId());
     }
-    if (!modelResolver.putModelSource(
-        model.getGroupId(), model.getArtifactId(), fileModelSource)) {
+    if (!modelResolver.putModelSource(model.getGroupId(), model.getArtifactId(), fileModelSource)) {
       return;
     }
 
@@ -265,8 +266,8 @@ public class Resolver {
       try {
         parentPom = new File(pomDirectory, parent.getRelativePath()).getCanonicalFile();
       } catch (IOException e) {
-        logger.warning("Unable to get canonical path of " + pomDirectory + " and "
-            + parent.getRelativePath());
+        logger.warning(
+            "Unable to get canonical path of " + pomDirectory + " and " + parent.getRelativePath());
         return;
       }
       if (parentPom.exists()) {
@@ -281,9 +282,9 @@ public class Resolver {
   }
 
   /**
-   * Adds the artifact to the map of deps, if it is not already there. Returns if the artifact
-   * was newly added. If the artifact was in the list at a different version, adds an comment
-   * about the desired version.
+   * Adds the artifact to the map of deps, if it is not already there. Returns if the artifact was
+   * newly added. If the artifact was in the list at a different version, adds an comment about the
+   * desired version.
    */
   private boolean addArtifact(Rule dependency, String parent) {
     String artifactName = dependency.name();
@@ -304,9 +305,7 @@ public class Resolver {
     return true;
   }
 
-  /**
-   * TODO: this should be removed once this uses Maven's own version resolution.
-   */
+  /** TODO: this should be removed once this uses Maven's own version resolution. */
   private void updateVersion(String artifactName, Rule dependency) {
     VersionRange versionRange;
     if (!restriction.containsKey(artifactName)) {
@@ -325,7 +324,8 @@ public class Resolver {
     if (!versionRange.containsVersion(new DefaultArtifactVersion(dependency.version()))) {
       try {
         dependency.setVersion(
-            versionResolver.resolveVersion(dependency.groupId(), dependency.artifactId(), versionRestriction));
+            versionResolver.resolveVersion(
+                dependency.groupId(), dependency.artifactId(), versionRestriction));
       } catch (InvalidArtifactCoordinateException e) {
         logger.warning("Error setting version: " + e.getLocalizedMessage());
       }
@@ -336,18 +336,12 @@ public class Resolver {
     return url.replaceAll(".pom$", "." + extension + ".sha1");
   }
 
-  /**
-   * Downloads the SHA-1 for the given artifact.
-   */
+  /** Downloads the SHA-1 for the given artifact. */
   private String downloadSha1(Rule rule) {
     String sha1Url = getSha1Url(rule.getUrl(), rule.getArtifact().getExtension());
     try {
-      HttpURLConnection connection = (HttpURLConnection) new URL(sha1Url).openConnection();
-      connection.setInstanceFollowRedirects(true);
-      connection.connect();
-      return extractSha1(
-          CharStreams.toString(
-              new InputStreamReader(connection.getInputStream(), Charset.defaultCharset())));
+      InputStream in = httpGet(sha1Url);
+      return extractSha1(CharStreams.toString(new InputStreamReader(in, Charset.defaultCharset())));
     } catch (IOException e) {
       logger.warning("Failed to download the sha1 at " + sha1Url);
     }
@@ -356,5 +350,12 @@ public class Resolver {
 
   static String extractSha1(String sha1Contents) {
     return sha1Contents.split("\\s+")[0];
+  }
+
+  protected InputStream httpGet(String url) throws IOException {
+    HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+    connection.setInstanceFollowRedirects(true);
+    connection.connect();
+    return connection.getInputStream();
   }
 }
